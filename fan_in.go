da2b836dec8dbe510f12dbc1067ff0bd376cbaa3
Mridul <mridulsahu01@gmail.com>
2019-01-09 21:20:25 +0530

    correction

--- fan_in.go
+++ fan_in.go
@@ -3,7 +3,11 @@ package main
 func BoringFanIn(chans ... <-chan string) <-chan string {
 	c := make(chan string)
 	for _, v := range chans {
-		go func(<-chan string) { c <- <-v }(v)
+		go func(<-chan string) {
+			for {
+				c <- <-v
+			}
+		}(v)
 	}
 	return c
 }
